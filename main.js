e0d2f8fe976f7c3f0c16d48edf6282cc37a307c8
Simon Bengtsson <sillen.00@hotmail.com>
2022-11-14 11:29:00 +0100

    First input option text and added button

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 window.addEventListener("DOMContentLoaded", main);
 
+const textElement = document.getElementById("text");
+const inputElement = document.getElementById("input-options")
+
+
 function main() {
-  
+
+};
+
+
+
+
+function firstChoice(){
+    
 };
