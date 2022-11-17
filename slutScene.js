f9048017c702e3b78da90b77ba699947c9c2722e
Simon Bengtsson <sillen.00@hotmail.com>
2022-11-17 14:54:23 +0100

    Update picture links

--- slutScene.js
+++ slutScene.js
@@ -1,7 +1,7 @@
 /**Slut scene med ny background image */
 function slutScene(){
     document.getElementById("option-buttons").replaceWith("");
-    document.body.style.backgroundImage = "url('/images/Grotta-under-vatten.jpg')";
+    document.body.style.backgroundImage = "url('./images/Grotta-under-vatten.jpg')";
 
     textElement.textContent = "Efter att tagit dig förbi hajen hittar du din bästa kompis i en undervattens grotta. Ni delar på det sista luften som är kvar i din dykartub och tar er snabbaste vägen, precis ut och upp till ytan och lever lyckliga i alla era dagar.";
 };
