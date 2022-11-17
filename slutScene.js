56f30bb67b33b58605de25652c29c2e5766a5963
Simon Bengtsson <sillen.00@hotmail.com>
2022-11-17 12:01:02 +0100

    Typo fix

--- slutScene.js
+++ slutScene.js
@@ -3,5 +3,5 @@ function slutScene(){
     document.getElementById("option-buttons").replaceWith("");
     document.body.style.backgroundImage = "url('/images/Grotta-under-vatten.jpg')";
 
-    textElement.textContent = "Efter att tagit dig förbi hajen hittar du din bästa kompis i en undervattens grotta. Ni delar på det sista luften som är kvar i din dykartub och tar er snabbaste vägen, precis ut och upp till ytan och lever lyckliga i alla sina dagar.";
+    textElement.textContent = "Efter att tagit dig förbi hajen hittar du din bästa kompis i en undervattens grotta. Ni delar på det sista luften som är kvar i din dykartub och tar er snabbaste vägen, precis ut och upp till ytan och lever lyckliga i alla era dagar.";
 };
