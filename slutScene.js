e7eaba7b959358794090e5a30354539b6526d4aa
Simon Bengtsson <sillen.00@hotmail.com>
2022-11-21 13:51:16 +0100

    Stavfel fix

--- slutScene.js
+++ slutScene.js
@@ -2,7 +2,7 @@
 function slutScene(){
     document.body.style.backgroundImage = "url('./images/Grotta-under-vatten.jpg')";
 
-    textElement.textContent = "Efter att tagit dig förbi hajen hittar du din bästa kompis i en undervattens grotta. Ni delar på det sista luften som är kvar i din dykartub och tar er snabbaste vägen, precis ut och upp till ytan och lever lyckliga i alla era dagar. Bra vänner är oftast mer värt än pengar!";
+    textElement.textContent = "Efter att tagit dig förbi hajen hittar du din bästa kompis i en undervattens grotta. Ni delar på det sista luften som är kvar i din dykartub och tar er snabbaste vägen ut och upp till ytan och lever lyckliga i alla era dagar. Bra vänner är oftast mer värt än pengar!";
 
 
     optionButtonElement1.textContent = "Grattis! Spela igen!";
